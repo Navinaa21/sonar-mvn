408343c2fef5a1efcb01d5e2ac70e8eeb1880ad2
Navinaa G <103243054+Navinaa21@users.noreply.github.com>
2025-01-03 21:36:48 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,16 +1,7 @@
 package com.example;
 
 public class Main {
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
-    public static String greet(String name) {
-        return "Hello, " + name + "!";
-    }
-
     public static void main(String[] args) {
-        System.out.println("Addition Result: " + add(5, 3));
-        System.out.println(greet("World"));
+        System.out.println("Hello world!");
     }
 }
